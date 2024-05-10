affeb467bbfb917b0784121e94f3b8cfb6a1a6b6
asadijaz123 <159795536+asadijaz123@users.noreply.github.com>
2024-05-10 22:54:06 +0500

    this final fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,4 @@
-import type { GetServerSideProps } from "next";
-import { HomePageServer } from "~/views/home"; 
-import { getRandomHeroActiveIndex } from "~/app/actions"; 
-import { BlogPostApi } from "~/app/api/api-helpers"; 
-
-export async function getServerSideProps(): Promise<
-  GetServerSideProps<{ activeIndex: any; posts: any }>
-> {
-  const activeIndex = await getRandomHeroActiveIndex();
-  const posts: any = await BlogPostApi.list({ limit: 3 }); 
-  return {
-    // @ts-ignore
-    props: {
-      activeIndex,
-      posts,
-    },
-  };
-}
+import HomePageServer from "~/views/home/home-server";
 
 export const metadata = {
   title: "All-In-One Marketing Content Platform | Media Shower",
@@ -23,12 +6,6 @@ export const metadata = {
     "Media Shower: The ultimate solution for marketing teams. Create, manage, and scale your content effortlessly. Transform from overworked to overjoyed today.",
 };
 
-export default function Home({
-  activeIndex,
-  posts,
-}: {
-  activeIndex: any;
-  posts: any;
-}) {
-  return <HomePageServer activeIndex={activeIndex} posts={posts} />;
+export default function Home() {
+  return <HomePageServer />;
 }
--- src/views/home/home-server.tsx
+++ src/views/home/home-server.tsx
@@ -1,11 +1,32 @@
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import { getRandomHeroActiveIndex } from "~/app/actions";
+import { BlogPostApi } from "~/app/api/api-helpers";
 import { HomePage } from "./home";
 
-export const HomePageServer = ({
-  activeIndex,
-  posts,
-}: {
-  activeIndex: number;
-  posts: any;
-}) => {
+const HomePageServer = () => {
+  const [activeIndex, setActiveIndex] = useState<any | null>(null);
+  const [posts, setPosts] = useState<any[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const index:any = await getRandomHeroActiveIndex();
+        const fetchedPosts:any = await BlogPostApi.list({ limit: 3 });
+        setActiveIndex(index);
+        setPosts(fetchedPosts);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+
   return <HomePage activeIndex={activeIndex} posts={posts} />;
 };
+
+export default HomePageServer;

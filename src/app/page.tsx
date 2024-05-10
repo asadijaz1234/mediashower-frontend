e328c1f2f722d01f0e8d52a8a6b7ba36e2cc91ea
asadijaz123 <159795536+asadijaz123@users.noreply.github.com>
2024-05-10 22:12:07 +0500

    this this

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import { HomePage } from "~/views/home";
-import HomePageServer from "~/views/home/home-server";
+import { HomePageServer } from "~/views/home";
+import { getRandomHeroActiveIndex } from "~/app/actions";
+import { BlogPostApi } from "~/app/api/api-helpers";
+export async function homePageServerSideProps() {
+  const activeIndex = await getRandomHeroActiveIndex();
+  const posts = await BlogPostApi.list({ limit: 3 });
+
+  return {
+    props: {
+      activeIndex,
+      posts,
+    },
+  };
+}
 
 export const metadata = {
   title: "All-In-One Marketing Content Platform | Media Shower",
@@ -7,6 +19,12 @@ export const metadata = {
     "Media Shower: The ultimate solution for marketing teams. Create, manage, and scale your content effortlessly. Transform from overworked to overjoyed today.",
 };
 
-export default function Home() {
-  return <HomePageServer />;
+export default function Home({
+  activeIndex,
+  posts,
+}: {
+  activeIndex: number;
+  posts: any;
+}) {
+  return <HomePageServer activeIndex={activeIndex} posts={posts} />;
 }
--- src/views/home/home-server.tsx
+++ src/views/home/home-server.tsx
@@ -1,33 +1,11 @@
-'use client'
-
-import React, { useState, useEffect } from 'react';
-import { getRandomHeroActiveIndex } from "~/app/actions";
-import { BlogPostApi } from "~/app/api/api-helpers";
 import { HomePage } from "./home";
 
-const HomePageServer = () => {
-  const [activeIndex, setActiveIndex] = useState<any | null>(null);
-  const [posts, setPosts] = useState<any[]>([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const index:any = await getRandomHeroActiveIndex();
-        const fetchedPosts:any = await BlogPostApi.list({ limit: 3 });
-        setActiveIndex(index);
-        setPosts(fetchedPosts);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-
-
+export const HomePageServer = ({
+  activeIndex,
+  posts,
+}: {
+  activeIndex: number;
+  posts: any;
+}) => {
   return <HomePage activeIndex={activeIndex} posts={posts} />;
 };
-
-export default HomePageServer;
-
